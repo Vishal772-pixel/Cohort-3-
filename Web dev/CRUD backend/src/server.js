baba9a7bcefce3b87cd4a940530c4a5ee58a7cab
Vishal772-pixel <fantasydark001@gmail.com>
2025-06-13 16:43:41 +0530

    refactor: remove deprecated mongoose connection options and improve server start log format

--- Web dev/CRUD backend/src/server.js
+++ Web dev/CRUD backend/src/server.js
@@ -38,6 +38,6 @@ app.use(errorHandler);
 
 // Start Server
 const PORT = process.env.PORT || 5000 ||3000;
-app.listen(PORT, () =>
-  console.log(`🚀 Server running on http://localhost:${PORT}`)
-);
+app.listen(PORT, () => {
+  console.log(` Server running on http://localhost:${PORT}`);
+});
